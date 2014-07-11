d3ddfe424116dca598b79794809e53931154dc9e
Mostafa Elhemali <mostafae@microsoft.com>
2014-07-11 13:00:36 -0700

    Basic Cassandra config class

---
+++ Libraries/Microsoft.Experimental.Azure.Cassandra/CassandraConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Microsoft.Experimental.Azure.Cassandra
+{
+	public sealed class CassandraConfig
+	{
+		private readonly string _clusterName;
+		private readonly ImmutableList<string> _dataDirectories;
+		private readonly string _commitLogDirectory;
+		private readonly string _savedCachesDirectory;
+		private readonly int _storagePort;
+		private readonly int _rpcPort;
+
+		public CassandraConfig(string clusterName, IEnumerable<string> dataDirectories,
+			string commitLogDirectory, string savedCachesDirectory,
+			int storagePort = 7000,
+			int rpcPort = 9160)
+		{
+			_clusterName = clusterName;
+			_dataDirectories = dataDirectories.ToImmutableList();
+			_commitLogDirectory = commitLogDirectory;
+			_savedCachesDirectory = savedCachesDirectory;
+			_storagePort = storagePort;
+			_rpcPort = rpcPort;
+		}
+
+		public void WriteToYamlFile(string filePath)
+		{
+			using (var writer = new StreamWriter(filePath, append: false, encoding: Encoding.ASCII))
+			{
+				WriteToYamlFile(writer);
+			}
+		}
+
+		public void WriteToYamlFile(TextWriter writer)
+		{
+			var serializer = new Serializer();
+			serializer.Serialize(writer, new
+			{
+				cluster_name = _clusterName,
+				data_file_directories = _dataDirectories.Select(d => d.Replace('\\', '/')).ToList(),
+				commitlog_directory = _commitLogDirectory.Replace('\\', '/'),
+				saved_caches_directory = _savedCachesDirectory.Replace('\\', '/'),
+				storage_port = _storagePort,
+				rpc_port = _rpcPort,
+			});
+		}
+	}
+}
---
+++ Tests/Microsoft.Experimental.Azure.Cassandra.Tests/CassandraConfigTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Experimental.Azure.Cassandra.Tests
+{
+	[TestClass]
+	public class CassandraConfigTest
+	{
+		[TestMethod]
+		public void TestSimpleConfig()
+		{
+			var config = new CassandraConfig(
+				clusterName: "Test Cluster",
+				dataDirectories: new[] { @"c:\d1", @"c:\d2" },
+				commitLogDirectory: @"C:\logs",
+				savedCachesDirectory: @"C:\SavedCaches");
+			string configOutput;
+			using (var writer = new StringWriter())
+			{
+				config.WriteToYamlFile(writer);
+				writer.Flush();
+				configOutput = writer.ToString();
+			}
+			Trace.WriteLine(configOutput);
+			StringAssert.Contains(configOutput, "cluster_name: Test Cluster");
+		}
+	}
+}
