056c99f0adac12871c2eea819e94d7da2fff304a
Mostafa Elhemali <mostafae@microsoft.com>
2014-07-14 14:52:46 -0700

    Do some cleanup work in anticipation of open-sourcing: a) Factor out common assembly properties into a single file, b) Fill out assembly descriptions and c) Standardize on warnings-as-errors and producing XML documentation for library projects

---
+++ Libraries/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Blue Coffee")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
