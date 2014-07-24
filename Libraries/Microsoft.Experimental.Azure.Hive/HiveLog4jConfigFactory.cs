ef5ac5029ec14c0009f5bc31df6d59e1cf268f11
Mostafa Elhemali <mostafae@microsoft.com>
2014-07-24 14:21:08 -0700

    Add a Hive library that can run Hive (just metastore for now)
    Former-commit-id: 98385debd7eda2395a943ce3d30b953a5c0f1b37

---
+++ Libraries/Microsoft.Experimental.Azure.Hive/HiveLog4jConfigFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Experimental.Azure.JavaPlatform.Log4j;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Experimental.Azure.Hive
+{
+	internal static class HiveLog4jConfigFactory
+	{
+		/// <summary>
+		/// Creates the default configuration.
+		/// </summary>
+		/// <param name="logDirectory">Directory for the log files.</param>
+		/// <returns>The configuration to use.</returns>
+		public static Log4jConfig CreateConfig(string logDirectory)
+		{
+			var layout = LayoutDefinition.PatternLayout("[%d{ISO8601}][%-5p][%-25c] %m%n");
+
+			var consoleAppender = AppenderDefinitionFactory.ConsoleAppender("console",
+				layout: layout);
+			var fileAppender = AppenderDefinitionFactory.DailyRollingFileAppender("file",
+				Path.Combine(logDirectory, "Hive.log"),
+				layout: layout);
+
+			var rootLogger = new RootLoggerDefinition(Log4jTraceLevel.INFO, consoleAppender, fileAppender);
+
+			return new Log4jConfig(rootLogger, Enumerable.Empty<ChildLoggerDefinition>());
+		}
+	}
+}
---
+++ Libraries/Microsoft.Experimental.Azure.Hive/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.Experimental.Azure.Hive")]
+[assembly: AssemblyDescription("This package can deploy Hive on an Azure worker node.")]
+
+[assembly: Guid("e59318b2-6da2-400f-bd6c-93a9da1250ca")]
