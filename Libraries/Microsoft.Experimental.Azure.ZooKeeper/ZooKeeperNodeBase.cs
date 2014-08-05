c57ee104a336c73a793f38cc45d8aea565010b9b
Mostafa Elhemali <mostafae@microsoft.com>
2014-08-05 12:53:32 -0700

    Factor the common code for running Zoo Keeper on Azure into an abstract base class in the Azure.ZooKeeper library
    Former-commit-id: 6eb5d41376268b63389185df25a766f93911db49 [formerly b6c40f14140d100effa08771dd149b9662b31c5b]
    Former-commit-id: 7b5cf67ba23c96a658747976d4d611ed68a1887a

---
+++ Libraries/Microsoft.Experimental.Azure.ZooKeeper/ZooKeeperNodeBase.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Experimental.Azure.JavaPlatform;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System.IO;
+
+namespace Microsoft.Experimental.Azure.ZooKeeper
+{
+	/// <summary>
+	/// The base class for a typical Azure Zoo Keeper node.
+	/// </summary>
+	public abstract class ZooKeeperNodeBase : NodeWithJavaBase
+	{
+		private ZooKeeperNodeRunner _nodeRunner;
+
+		/// <summary>
+		/// Overrides the Run method to run Zoo Keeper.
+		/// </summary>
+		protected override void GuardedRun()
+		{
+			_nodeRunner.Run();
+		}
+
+		/// <summary>
+		/// Overrides initialization to setup Zoo Keeper.
+		/// </summary>
+		protected override void PostJavaInstallInitialize()
+		{
+			InstallZooKeeper();
+		}
+
+		/// <summary>
+		/// Gets the data directory - by default we look for a "DataDirectory" local resource.
+		/// </summary>
+		protected virtual string DataDirectory
+		{
+			get { return RoleEnvironment.GetLocalResource("DataDirectory").RootPath; }
+		}
+
+		private void InstallZooKeeper()
+		{
+			_nodeRunner = new ZooKeeperNodeRunner(
+				dataDirectory: Path.Combine(DataDirectory, "Data"),
+				configsDirectory: Path.Combine(DataDirectory, "Config"),
+				logsDirectory: Path.Combine(DataDirectory, "Logs"),
+				jarsDirectory: Path.Combine(InstallDirectory, "Jars"),
+				javaHome: JavaHome);
+			_nodeRunner.Setup();
+		}
+	}
+}
--- TestServices/Kafkaesque/ZooKeeperNode/WorkerRole.cs
+++ TestServices/Kafkaesque/ZooKeeperNode/WorkerRole.cs
@@ -1,48 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
-using System.IO;
-using Microsoft.Experimental.Azure.JavaPlatform;
-using System.IO.Compression;
 using Microsoft.Experimental.Azure.ZooKeeper;
 
 namespace Zookeeper
 {
-	public class WorkerRole : NodeWithJavaBase
+	public class WorkerRole : ZooKeeperNodeBase
 	{
-		private ZooKeeperNodeRunner _nodeRunner;
-
-		protected override void GuardedRun()
-		{
-			_nodeRunner.Run();
-		}
-
-		protected override void PostJavaInstallInitialize()
-		{
-			InstallZooKeeper();
-		}
-
-		private void InstallZooKeeper()
-		{
-			_nodeRunner = new ZooKeeperNodeRunner(
-				dataDirectory: Path.Combine(DataDirectory, "Data"),
-				configsDirectory: Path.Combine(DataDirectory, "Config"),
-				logsDirectory: Path.Combine(DataDirectory, "Logs"),
-				jarsDirectory: Path.Combine(InstallDirectory, "Jars"),
-				javaHome: JavaHome);
-			_nodeRunner.Setup();
-		}
-
-		private static string DataDirectory
-		{
-			get { return RoleEnvironment.GetLocalResource("DataDir").RootPath; }
-		}
 	}
 }
