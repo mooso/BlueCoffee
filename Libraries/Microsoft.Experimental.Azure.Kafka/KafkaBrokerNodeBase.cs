e606dd516e7e2d4314618c767b2dbf6681165960
Mostafa Elhemali <mostafae@microsoft.com>
2014-08-05 13:03:07 -0700

    Factor the common code for running Kafka on Azure into an abstract base class in the Azure.Kafka library
    Former-commit-id: 8662bac2864d2318a7226cb7794f3d811baefcfd [formerly 1dd51452c1245d19c7cf760eb92a7f204188e8b3]
    Former-commit-id: c1b81d7b7f64658d878658fb5c59ed2bda601634

---
+++ Libraries/Microsoft.Experimental.Azure.Kafka/KafkaBrokerNodeBase.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Experimental.Azure.JavaPlatform;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Experimental.Azure.Kafka
+{
+	/// <summary>
+	/// The base class for a typical Azure Kafka broker node.
+	/// </summary>
+	public abstract class KafkaBrokerNodeBase : NodeWithJavaBase
+	{
+		private KafkaBrokerRunner _kafkaRunner;
+
+		/// <summary>
+		/// The port that the ZooKeeper nodes are listening on. Defaults to 2181.
+		/// </summary>
+		protected virtual int ZooKeeperPort { get { return 2181; } }
+
+		/// <summary>
+		/// The Azure role for the Zoo Keeper nodes. Defaults to the role "ZooKeeperNode".
+		/// </summary>
+		protected virtual Role ZooKeeperRole
+		{
+			get
+			{
+				return RoleEnvironment.Roles["ZooKeeperNode"];
+			}
+		}
+
+		/// <summary>
+		/// Discovers the Zoo Keeper node addresses. Defaults to all the nodes in the ZooKeeperRole.
+		/// </summary>
+		/// <returns>The list of IP addresses for Zoo Keeper nodes.</returns>
+		protected virtual IEnumerable<string> DiscoverZooKeeperHosts()
+		{
+			return ZooKeeperRole.Instances.Select(GetIPAddress);
+		}
+
+		/// <summary>
+		/// Gets the data directory - by default we look for a "DataDirectory" local resource.
+		/// </summary>
+		protected virtual string DataDirectory
+		{
+			get { return RoleEnvironment.GetLocalResource("DataDirectory").RootPath; }
+		}
+
+		/// <summary>
+		/// Overrides the Run method to run Kafka.
+		/// </summary>
+		protected sealed override void GuardedRun()
+		{
+			_kafkaRunner.Run();
+		}
+
+		/// <summary>
+		/// Overrides initialization to setup Kafka.
+		/// </summary>
+		protected sealed override void PostJavaInstallInitialize()
+		{
+			InstallKafka();
+		}
+
+		private void InstallKafka()
+		{
+			var zookeeperHosts = DiscoverZooKeeperHosts();
+			var myBrokerId = Int32.Parse(RoleEnvironment.CurrentRoleInstance.Id.Split('_').Last());
+			_kafkaRunner = new KafkaBrokerRunner(
+				dataDirectory: Path.Combine(DataDirectory, "Data"),
+				configsDirectory: Path.Combine(DataDirectory, "Config"),
+				logsDirectory: Path.Combine(DataDirectory, "Logs"),
+				jarsDirectory: Path.Combine(InstallDirectory, "Jars"),
+				zooKeeperHosts: zookeeperHosts,
+				zooKeeperPort: ZooKeeperPort,
+				brokerId: myBrokerId,
+				javaHome: JavaHome);
+			_kafkaRunner.Setup();
+		}
+	}
+}
--- TestServices/Kafkaesque/KafkaBroker/WorkerRole.cs
+++ TestServices/Kafkaesque/KafkaBroker/WorkerRole.cs
@@ -1,55 +1,7 @@
-using Microsoft.Experimental.Azure.JavaPlatform;
 using Microsoft.Experimental.Azure.Kafka;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Net;
-using System.Threading;
 
 namespace KafkaBroker
 {
-	public class WorkerRole : NodeWithJavaBase
-	{
-		private KafkaBrokerRunner _kafkaRunner;
-		private const int ZooKeeperPort = 2181;
-
-		protected override void GuardedRun()
-		{
-			_kafkaRunner.Run();
-		}
-
-		protected override void PostJavaInstallInitialize()
-		{
-			InstallKafka();
-		}
-
-		private void InstallKafka()
-		{
-			var zookeeperRole = RoleEnvironment.Roles["ZooKeeperNode"];
-			var zookeeperHosts = zookeeperRole.Instances.Select(i => i.InstanceEndpoints.First().Value.IPEndpoint.Address.ToString());
-			var myBrokerId = Int32.Parse(RoleEnvironment.CurrentRoleInstance.Id.Split('_').Last());
-			_kafkaRunner = new KafkaBrokerRunner(
-				dataDirectory: Path.Combine(DataDirectory, "Data"),
-				configsDirectory: Path.Combine(DataDirectory, "Config"),
-				logsDirectory: Path.Combine(DataDirectory, "Logs"),
-				jarsDirectory: Path.Combine(InstallDirectory, "Jars"),
-				zooKeeperHosts: zookeeperHosts,
-				zooKeeperPort: ZooKeeperPort,
-				brokerId: myBrokerId,
-				javaHome: JavaHome);
-			_kafkaRunner.Setup();
-		}
-
-		private static string DataDirectory
-		{
-			get { return RoleEnvironment.GetLocalResource("DataDir").RootPath; }
-		}
-	}
+	public class WorkerRole : KafkaBrokerNodeBase
+	{ }
 }
