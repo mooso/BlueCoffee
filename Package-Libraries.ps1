b3909bf8244443dfb2ea3610e068cebf09ac09ef
Mostafa Elhemali <mostafae@microsoft.com>
2014-08-15 13:51:23 -0700

    Bump nuget version numbers for Spark to signify that it now has Shark, and its dependencies. I just uploaded these packages to nuget so this marks the true 1.6 version.
    Former-commit-id: e09ea2970d632c7237c5e0202437a376dc4ef7df [formerly 3e6d932688f5c99d8493d3d289e254ff3a2e0598]
    Former-commit-id: 1e8d7576ebe8a3fa30bdb74f428a9a08e4e54831

--- Package-Libraries.ps1
+++ Package-Libraries.ps1
@@ -1,9 +1,12 @@
-﻿Remove-Item -Force -Recurse .\FinalPackages
+﻿if (Test-Path .\FinalPackages)
+{
+    Remove-Item -Force -Recurse .\FinalPackages
+}
 $finalPackagesDir = md .\FinalPackages
-Get-ChildItem -Recurse -Include *.nuspec | %{ `
+Get-ChildItem Libraries -Recurse -Include *.nuspec | %{ `
     $dir = Split-Path $_
     pushd $dir
     $projectFile = Get-ChildItem *.csproj
-    nuget pack -OutputDirectory $finalPackagesDir -Symbols -Properties 'Configuration=Release' -Build $projectFile
+    nuget pack -OutputDirectory $finalPackagesDir -Symbols -Properties 'Configuration=Release;Platform=AnyCPU' -Build $projectFile
     popd
 }
