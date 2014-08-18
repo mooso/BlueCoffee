6ac790a632133abab46a2a653c6e39ab3b3439ab
Mostafa Elhemali <mostafae@microsoft.com>
2014-08-18 14:02:54 -0700

    Overhaul how I package the nuget packages so that I put the jar files into them after I removed them from the assembly itself. I added a hacky script to upload them into a storage account as well, but honestly I don't really like how the script is packaged today.

---
+++ ClientScripts/Upload-ResourcesToContext.ps1
@@ -0,0 +1,21 @@
+﻿param([Parameter(Mandatory=$true)][Microsoft.WindowsAzure.Commands.Storage.Model.ResourceModel.AzureStorageContext]$storageContext,
+    $projectRoot = '.',
+    $container = 'bluecoffeeresources')
+{
+	Write-Host "Uploading resources..."
+    $containerReference = New-AzureStorageContainer -Name $container -Context $storageContext -ErrorAction SilentlyContinue
+    $resourceDirectories = Get-ChildItem "$projectRoot\BlueCoffeeResources"
+    $resourceDirectories | %{
+        $blobNamePrefix = $_.Name + "/"
+        $myResources = Get-ChildItem "$($_.FullName)"
+        $myResources | %{
+            $blobName = $blobNamePrefix + $_.Name
+            $existingBlob = Get-AzureStorageBlob -Blob $blobName -Context $storageContext -Container $container -ErrorAction SilentlyContinue
+            if (($existingBlob -eq $null) -or ($existingBlob.Length -ne $_.Length))
+            {
+                Write-Host "Uploading $blobName ..."
+                $newBlob = Set-AzureStorageBlobContent -Blob $blobName -Context $storageContext -Container $container -File $($_.FullName)
+            }
+        }
+    }
+}
---
+++ Scripts/Package-Libraries.ps1
@@ -0,0 +1,58 @@
+﻿function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+function Recreate-Directory($directory)
+{
+    if (Test-Path $directory)
+    {
+        Remove-Item -Force -Recurse $directory
+    }
+    $md = md $directory
+}
+
+function Get-AssemblyDescription($assemblyPath)
+{
+    $assembly = [System.Reflection.Assembly]::ReflectionOnlyLoadFrom($assemblyPath)
+    $attributes = [System.Reflection.CustomAttributeData]::GetCustomAttributes($assembly)
+    $descriptionAttribute = $attributes | ?{$_.AttributeType.Name -eq "AssemblyDescriptionAttribute"}
+    $descriptionAttribute.ConstructorArguments[0].Value
+}
+
+$rootDirectory = Split-Path $(Get-ScriptDirectory)
+$packageDirectory = "$rootDirectory\FinalPackages"
+$stagingDirectory = "$rootDirectory\PackageStaging"
+$nugetVersion = Get-Content -Raw "$rootDirectory\Libraries\NugetVersion.txt"
+
+Recreate-Directory $packageDirectory
+
+Get-ChildItem "$rootDirectory\Libraries" -Recurse -Include *.nuspec | %{
+    $dir = Split-Path $_
+    pushd $dir
+    $projectFile = Get-ChildItem *.csproj
+    $buildOutput = msbuild $projectFile /p:Configuration="Release" /p:Platform="AnyCPU" /p:VisualStudioVersion="12.0"
+    popd
+	if ($LASTEXITCODE -ne 0)
+	{
+		$buildOutput | Write-Host
+        return
+	}
+    $libraryName = $projectFile.BaseName
+    $libSimpleName = $libraryName.Substring("Microsoft.Experimental.Azure.".Length)
+    $assemblyDescription = Get-AssemblyDescription "$dir\bin\Release\$libraryName.dll"
+    Recreate-Directory $stagingDirectory
+    $nuspecContent = Get-Content -Raw "$dir\$libraryName.nuspec"
+    $nuspecContent = $nuspecContent.Replace('$id$', $libraryName).Replace('$title$', $libraryName).Replace('$description$', $assemblyDescription).Replace('$nugetversion$', $nugetVersion)
+    $nuspecContent | Out-File -FilePath "$stagingDirectory\$libraryName.nuspec"
+    $libDir = md "$stagingDirectory\lib\net45"
+    Copy-Item "$dir\bin\Release\$libraryName.*" $libDir
+    $resourcesDir = md "$stagingDirectory\content\net45\BlueCoffeeResources\$libSimpleName"
+    Copy-Item "$dir\Resources\*.zip" $resourcesDir
+    Copy-Item "$rootDirectory\ClientScripts\*.ps1" "$stagingDirectory\content\net45"
+    pushd $stagingDirectory
+    nuget pack -OutputDirectory $packageDirectory
+    popd
+    Remove-Item -Recurse -Force $stagingDirectory
+}
