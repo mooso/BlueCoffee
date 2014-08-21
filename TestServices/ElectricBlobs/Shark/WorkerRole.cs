7d107e726bce4abe72923291350f054a2e04f21a
Mostafa Elhemali <mostafae@microsoft.com>
2014-08-21 10:48:57 -0700

    Put the scratch and warehouse directories up on WASB in the Shark test service. This gets CTAS to work.

--- TestServices/ElectricBlobs/Shark/WorkerRole.cs
+++ TestServices/ElectricBlobs/Shark/WorkerRole.cs
@@ -19,7 +19,11 @@ namespace Shark
 	{
 		protected override ImmutableDictionary<string, string> GetHadoopConfigProperties()
 		{
-			return WasbConfiguration.GetWasbConfigKeys();
+			var rootFs = String.Format("wasb://shark@{0}.blob.core.windows.net",
+					WasbConfiguration.ReadWasbAccountsFile().First());
+			return WasbConfiguration.GetWasbConfigKeys()
+				.Add("hive.exec.scratchdir", rootFs + "/scratch")
+				.Add("hive.metastore.warehouse.dir", rootFs + "/warehouse");
 		}
 	}
 }
