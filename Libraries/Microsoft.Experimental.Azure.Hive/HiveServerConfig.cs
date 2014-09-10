60e3428024a743f0f5472f0e5a59bfbf9cdbceae
Mostafa Elhemali <mostafae@microsoft.com>
2014-09-10 09:01:57 -0700

    Add freeform extra properties to HiveServerConfig

--- Libraries/Microsoft.Experimental.Azure.Hive/HiveServerConfig.cs
+++ Libraries/Microsoft.Experimental.Azure.Hive/HiveServerConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Experimental.Azure.JavaPlatform;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,16 +15,19 @@ namespace Microsoft.Experimental.Azure.Hive
 	{
 		private readonly int _port;
 		private readonly string _metastoreUris;
+		private readonly ImmutableDictionary<string, string> _extraProperties;
 
 		/// <summary>
 		/// Creates the config.
 		/// </summary>
 		/// <param name="port">The port to listen on.</param>
 		/// <param name="metastoreUris">The URI of the metastore service to connect to.</param>
-		public HiveServerConfig(int port, string metastoreUris)
+		/// <param name="extraProperties">Any extra configuration properties to set.</param>
+		public HiveServerConfig(int port, string metastoreUris, ImmutableDictionary<string, string> extraProperties = null)
 		{
 			_port = port;
 			_metastoreUris = metastoreUris;
+			_extraProperties = extraProperties ?? ImmutableDictionary<string, string>.Empty;
 		}
 
 		/// <summary>
@@ -33,11 +37,11 @@ namespace Microsoft.Experimental.Azure.Hive
 		{
 			get
 			{
-				return new Dictionary<string, string>()
+				return _extraProperties.Concat(new Dictionary<string, string>()
 				{
 					{ "hive.metastore.uris", _metastoreUris },
 					{ "hive.server2.thrift.port", _port.ToString() },
-				};
+				});
 			}
 		}
 	}
