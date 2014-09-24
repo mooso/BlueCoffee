f66ff232ac30afcdd5b34f6090a6e9424947440f
Mostafa Elhemali <mostafae@microsoft.com>
2014-09-24 15:14:53 -0700

    Mostly empty projects for adding Storm. Seeded with the jars from storm-0.9.2-incubating (unmodified)

---
+++ Libraries/Microsoft.Experimental.Azure.Storm/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Experimental.Azure.Storm")]
+[assembly: AssemblyDescription("This package can deploy Storm on an Azure worker node.")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27980602-559d-47ba-946f-1d34ac2342d0")]
---
+++ Scripts/Create-StormJars.ps1
@@ -0,0 +1,29 @@
+﻿Param ([Parameter(Mandatory=$true)] $StormHome)
+
+# Find out where I am
+$Invocation = (Get-Variable MyInvocation).Value
+$scriptLocation = Split-Path $Invocation.MyCommand.Path
+
+# Put all the jars in a single directory
+$jars = Get-ChildItem "$StormHome\lib","$StormHome\external" -Recurse -Include *.jar
+$tempDir = md "$env:TEMP\StormJarsToZip"
+$jars | %{Copy-Item $_ $tempDir}
+
+# Find out where to create the zip file
+$zipFileName = "$scriptLocation\..\Libraries\Microsoft.Experimental.Azure.Storm\Resources\Jars.zip"
+if (Test-Path $zipFileName)
+{
+    Remove-Item -Force $zipFileName
+}
+
+# Create the zip file
+Add-Type -Assembly System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+[System.IO.Compression.ZipFile]::CreateFromDirectory($tempDir, $zipfilename, $compressionLevel, $false)
+
+# Cleanup the temp directory
+Remove-Item -Recurse -Force $tempDir
+
+# Split up the zip file
+#Add-Type -TypeDefinition $(Get-Content "$scriptLocation\..\Libraries\CombineJar.cs" -Raw) -Language CSharp
+#[SplitAndMerge.Program]::Split($zipfilename)
