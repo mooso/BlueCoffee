e7c1eff62230820ef6c478cefaecede1012bc51a
Mostafa Elhemali <mostafae@microsoft.com>
2014-10-06 13:56:59 -0700

    ASP web UI (stub) for querying the DRPC endpoint in the test Storm cluster.

---
+++ TestServices/StormyKafka/StormDrpcWebUI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using StormDrpcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StormDrpcWebUI.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Query(StormDrpcQuery model)
+		{
+			if (ModelState.IsValid)
+			{
+				model.QueryResult = 5; // Stub for now, will fill.
+				return View("Index", model);
+			}
+			return View("Index", model);
+		}
+	}
+}
---
+++ TestServices/StormyKafka/StormDrpcWebUI/Models/StormDrpcQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StormDrpcWebUI.Models
+{
+	public class StormDrpcQuery
+	{
+		[Required]
+		[Display(Name = "Space-separated word list")]
+		public string QueryWords { get; set; }
+		public int? QueryResult { get; set; }
+	}
+}
