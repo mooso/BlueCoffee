1245de353f7348a8475a951464f5b40980e42809
Mostafa Elhemali <mostafae@microsoft.com>
2014-11-10 13:58:38 -0800

    Add a failing test to prove that I can't connect two ZK nodes properly today.

--- Tests/Microsoft.Experimental.Azure.ZooKeeper.Tests/TwoNodeZooKeeperTester.java
+++ Tests/Microsoft.Experimental.Azure.ZooKeeper.Tests/TwoNodeZooKeeperTester.java
@@ -4,20 +4,22 @@ import java.util.*;
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.ZooDefs.Ids;
 
-public class SimpleZooKeeperTester {
+public class TwoNodeZooKeeperTester {
 	public static void main(String[] args) throws Exception {
-		ZooKeeper zk = new ZooKeeper("localhost:2181", 3000, null);
+		ZooKeeper zk1 = new ZooKeeper(args[0], 3000, null);
+		ZooKeeper zk2 = new ZooKeeper(args[1], 3000, null);
 		String path = "/simpletest";
-		if (zk.exists(path, false) != null) {
+		if (zk2.exists(path, false) != null) {
 			System.err.println("Node already exists!");
 			return;
 		}
-		zk.create(path, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-		if (zk.exists(path, false) == null) {
+		zk1.create(path, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+		if (zk2.exists(path, false) == null) {
 			System.err.println("Node doesn't exist after creation!");
 			return;
 		}
-		zk.close();
+		zk1.close();
+		zk2.close();
 		System.out.println("Success");
 	}
 }
--- Tests/Microsoft.Experimental.Azure.ZooKeeper.Tests/ZooKeeperEndToEndTests.cs
+++ Tests/Microsoft.Experimental.Azure.ZooKeeper.Tests/ZooKeeperEndToEndTests.cs
@@ -21,18 +21,10 @@ namespace Microsoft.Experimental.Azure.ZooKeeper.Tests
 			{
 				Directory.Delete(tempDirectory, recursive: true);
 			}
+			var killer = new ProcessKiller();
 			var zooKeeperDirectory = Path.Combine(tempDirectory, "ZooKeeper");
 			var jarsDirectory = Path.Combine(zooKeeperDirectory, "lib");
-			var zooKeeperRunner = new ZooKeeperNodeRunner(
-					resourceFileDirectory: ResourcePaths.ZooKeeperResourcesPath,
-					config: new ZooKeeperConfig(Path.Combine(zooKeeperDirectory, "data")),
-					configsDirectory: Path.Combine(zooKeeperDirectory, "conf"),
-					logsDirectory: Path.Combine(zooKeeperDirectory, "log"),
-					jarsDirectory: jarsDirectory,
-					javaHome: TestJavaRunner.JavaHome);
-			zooKeeperRunner.Setup();
-			var killer = new ProcessKiller();
-			var zooKeeperTask = Task.Factory.StartNew(() => zooKeeperRunner.Run(false, killer));
+			var zooKeeperTask = RunZooKeeper(killer, zooKeeperDirectory);
 			try
 			{
 				var output = TestJavaRunner.RunJavaResourceFile(
@@ -46,6 +38,51 @@ namespace Microsoft.Experimental.Azure.ZooKeeper.Tests
 				killer.KillAll();
 				Task.WaitAll(zooKeeperTask);
 			}
+		}
+
+		[TestMethod]
+		[Ignore]
+		public void TwoZooKeeperNodesTest()
+		{
+			var tempDirectory = @"C:\ZooKeeperTestOutput";
+			if (Directory.Exists(tempDirectory))
+			{
+				Directory.Delete(tempDirectory, recursive: true);
+			}
+			var killer = new ProcessKiller();
+			var zooKeeper1Directory = Path.Combine(tempDirectory, "ZooKeeper1");
+			var zooKeeper2Directory = Path.Combine(tempDirectory, "ZooKeeper2");
+			var jarsDirectory = Path.Combine(zooKeeper1Directory, "lib");
+			var zooKeeper1Task = RunZooKeeper(killer, zooKeeper1Directory);
+			var zooKeeper2Task = RunZooKeeper(killer, zooKeeper2Directory, port: 2182);
+			try
+			{
+				var output = TestJavaRunner.RunJavaResourceFile(
+					testClass: GetType(),
+					javaResourceName: "TwoNodeZooKeeperTester.java",
+					libDirectory: jarsDirectory,
+					arguments: "localhost:2181 localhost:2182");
+				Assert.AreEqual("Success", output.StandardOutput.Trim(), output.StandardError);
+			}
+			finally
+			{
+				killer.KillAll();
+				Task.WaitAll(zooKeeper1Task, zooKeeper2Task);
+			}
+		}
+
+		private static Task RunZooKeeper(ProcessKiller killer, string zooKeeperDirectory, int port = 2181)
+		{
+			var zooKeeperRunner = new ZooKeeperNodeRunner(
+					resourceFileDirectory: ResourcePaths.ZooKeeperResourcesPath,
+					config: new ZooKeeperConfig(Path.Combine(zooKeeperDirectory, "data"), port: port),
+					configsDirectory: Path.Combine(zooKeeperDirectory, "conf"),
+					logsDirectory: Path.Combine(zooKeeperDirectory, "log"),
+					jarsDirectory: Path.Combine(zooKeeperDirectory, "lib"),
+					javaHome: TestJavaRunner.JavaHome);
+			zooKeeperRunner.Setup();
+			var zooKeeperTask = Task.Factory.StartNew(() => zooKeeperRunner.Run(false, killer));
+			return zooKeeperTask;
 		}
 	}
 }
